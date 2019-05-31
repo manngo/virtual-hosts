5a5475b78823696dcedbaa0d4a5a38a45f826963
manngo <mark@manngo.net>
2019-05-31 17:02:38 +1000

    Added Button to Select Root Folder
    Need to build executables again …

--- scripts/do-virtual-hosts.js
+++ scripts/do-virtual-hosts.js
@@ -1,12 +1,22 @@
 /*	Do Virtual Hosts
 	================================================ */
 //	document.addEventListener('DOMContentLoaded',doVirtualHosts,false);
+	const { dialog } = require('electron').remote;
 
 	var exported=require('./main.js');
 	var {os,hosts,platforms,setLineNumbers,platform,test}=exported;
 	doVirtualHosts();
 	function doVirtualHosts() {
 		var form=document.querySelector('form#generator');
+form.elements['path'].onclick=function(event) {
+	event.preventDefault();
+	dialog.showOpenDialog({
+		properties: ['openDirectory']
+	},filePaths=>{
+//		console.log(filePaths.toString());
+		form.elements['root'].value=filePaths.toString();
+	});
+};
 		form['generate'].onclick=form['content'].onclick=doit;
 		//	Restore Values
 			var data=localStorage.getItem('data');
